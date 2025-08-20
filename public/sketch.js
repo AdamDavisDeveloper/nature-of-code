d59d0186c1aea70f7757d7b8cd441c8b30c4c3d7
AdamDavisDeveloper <adavisdeveloper@gmail.com>
2025-08-20 00:38:37 -0700

    created a random walker!

--- public/sketch.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-  createCanvas(900, 800);
-}
-function draw() {
-  background(20);
-  circle(width/2, height/2, 120);
-}
-
---
+++ public/walker.js
@@ -0,0 +1,60 @@
+let positionX;
+let positionY;
+
+let wallyX;
+let wallyY;
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+function randCoord() { // number[]
+  let x = getRandomInt(0, 2);
+  let y = getRandomInt(0, 2);
+  if(x === 0) x--;
+  if(y === 0) y--;
+  return [x, y];
+}
+
+function setup() {
+  createCanvas(900, 800);
+  background(20);
+
+  positionX = width / 2;
+  positionY = height / 2;
+
+  wallyX = (width / 2) - 250;
+  wallyY = (height / 2) - 150;
+};
+
+function draw() {
+  // getting a direction to move the dot
+  const newCoords = randCoord();
+  const newX = newCoords[0];
+  const newY = newCoords[1];
+
+  const wallyNewCoords = randCoord();
+  const newWallyX = wallyNewCoords[0];
+  const newWallyY = wallyNewCoords[1];
+
+  fill(255);
+  circle(positionX, positionY, 1);
+
+  fill(255, 192, 203);
+  noStroke();
+  circle(wallyX, wallyY, 1);
+
+
+  // A (-1,-1) -- 
+  // B (+1,-1) --
+  // C (+1,-1) --
+  // D (+1,+1) --
+
+  // Setting new dot direction for one frame
+  positionX += newX;
+  positionY += newY;
+  wallyX += newWallyX;
+  wallyY += newWallyY;
+};
